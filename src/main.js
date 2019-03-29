d1560c5b3398da6848f6da74fa94d84c3a1df64e
sijia.LIIU <307933425@qq.com>
2019-03-29 19:12:16 +0800

    add icons

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'	
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
-import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 Vue.use(VueAwesomeSwiper)
