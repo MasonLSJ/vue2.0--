96159a90fc245a5d44f4b9d348d643767a69963e
sijia.LIIU <307933425@qq.com>
2019-04-02 11:18:02 +0800

    banner finish

--- src/pages/home/components/Recomend.vue
+++ src/pages/home/components/Recomend.vue
@@ -2,7 +2,8 @@
 <div>
 	<div class="title">热门推荐</div>
 	<ul>
-		<li class="item border-bottom" 
+		<router-link tag="li" :to="'/detail/' + item.id"
+		 class="item border-bottom" 
 			v-for="item of list"
 			:key="item.id"
 			>
@@ -12,7 +13,7 @@
 				<p class="item-desc">{{item.desc}}</p>
 				<button class="item-button">查看详情</button>
 			</div>
-		</li>
+		</router-link>
 	</ul>
 </div>
 </template>
